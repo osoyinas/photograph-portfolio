eff357a5cffab89c18b76a48ca6c150374b85cd0
osoyinas <osoyinas@gmail.com>
2022-09-16 21:13:53 +0200

    arreglos

---
+++ imageSelector.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".image-container .image img").forEach(image => {
+    image.addEventListener('click',() => {
+        document.querySelector(".popup-image").style.display = 'block';
+        document.querySelector(".popup-image img").src = image.getAttribute('src');
+    }) 
+});
+document.querySelector(".close").addEventListener('click',() => {
+    document.querySelector(".popup-image").style.display = 'none';
+})
