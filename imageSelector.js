f528fbf6cf3e6a549323b2ca7172d36c6b6b75a0
osoyinas <osoyinas@gmail.com>
2022-09-17 11:50:42 +0200

    animations

--- imageSelector.js
+++ imageSelector.js
@@ -1,12 +1,45 @@
 document.querySelectorAll(".image-container .image img").forEach(image => {
-    image.addEventListener('click',() => {
+    image.addEventListener('click', () => {
         document.querySelector(".popup-image").style.display = 'block';
         document.querySelector(".popup-image img").src = image.getAttribute('src');
-    }) 
+    })
 });
-document.querySelector(".close").addEventListener('click',() => {
+document.querySelector(".close").addEventListener('click', () => {
     document.querySelector(".popup-image").style.display = 'none';
 });
-document.querySelector(".popup-image img").addEventListener('click',() => {
+document.querySelector(".popup-image img").addEventListener('click', () => {
     document.querySelector(".popup-image").style.display = 'none';
+})
+
+
+document.querySelectorAll(".image-container .image").forEach(image=>{
+    image.classList.add("fade-in");
+})
+const faders = document.querySelectorAll('.fade-in');
+const sliders=document.querySelectorAll('.slide-in');
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -150px 0px"
+
+};
+const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            return;
+        }else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider=>{
+    appearOnScroll.observe(slider);
 })
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll",()=>{
     }
     if(currentScroll>lastScroll && !nav.classList.contains("scroll-down")){
         nav.classList.add("scroll-down");
-        nav.classList.add("scroll-up");
+        nav.classList.remove("scroll-up");
         
     }
     if(currentScroll<lastScroll && nav.classList.contains("scroll-down")){
@@ -15,7 +15,13 @@ window.addEventListener("scroll",()=>{
         nav.classList.add("scroll-up");
     
     }
-
-
     lastScroll=currentScroll;
+});
+const container=document.querySelector(".containerSabermas")
+const btnSaberM=document.querySelector(".sabermasBtn")
+btnSaberM.addEventListener('mouseenter',()=>{
+    container.classList.add("levitate");
+});
+btnSaberM.addEventListener('mouseout',()=>{
+    container.classList.remove("levitate");
 })
